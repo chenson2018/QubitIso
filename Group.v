7d98423d85d2b5d87387c969e2abf358c788cf38
Chris Henson <chrishenson.net@gmail.com>
2023-11-03 13:14:17 -0400

    implicit args in group axioms

--- Group.v
+++ Group.v
@@ -24,10 +24,10 @@ Qed.
 Class PredicateGroup (G: Type) (op: group_binop G) (pred: G -> Prop) (group_eq: group_eq_rel G) (e: equiv G group_eq): Type := {
         id : G
       ; inverse: G -> G
-      ; id_left: forall x, pred x -> id * x == x
-      ; id_right: forall x, pred x -> x * id == x
-      ; assoc: forall x y z, pred x -> pred y -> pred z -> (x * y) * z == x * (y * z)
-      ; right_inv: forall x, pred x -> x * (inverse x) == id
-      ; op_closed: forall x y, pred x -> pred y -> pred (x * y)
-      ; inverse_closed: forall x, pred x -> pred (inverse x)
+      ; id_left {x}: pred x -> id * x == x
+      ; id_right {x}: pred x -> x * id == x
+      ; assoc {x y z}: pred x -> pred y -> pred z -> (x * y) * z == x * (y * z)
+      ; right_inv {x}: pred x -> x * (inverse x) == id
+      ; op_closed {x y}: pred x -> pred y -> pred (x * y)
+      ; inverse_closed {x}: pred x -> pred (inverse x)
 }.
